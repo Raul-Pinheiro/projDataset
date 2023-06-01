f713bcebc273c389589e81aa5deb476fc3338891
Raul Pinheiro <raulmatheus.rm@gmail.com>
2023-05-31 21:57:03 -0300

    New sunburst chart

--- main.py
+++ main.py
@@ -3,36 +3,31 @@ from matplotlib import pyplot
 import matplotlib
 import os
 import squarify
+import plotly.express as px
 
 arquivo = "./Car_Models.csv"
 
 
-def grafico_de_arvore(sample: int = 10):
+def grafico_sunburst(amostragem: int = 100):
+    """['Company', 'Model', 'Horsepower', 'Torque', 'Transmission Type', 'Drivetrain', 'Fuel Economy', 'Number of Doors', 'Price', 'Model Year Range', 'Body Type', 'Engine Type', 'Number of Cylinders']"""
+
     output = os.path.join(arquivo)
 
-    planilha = pd.read_csv(output, nrows=sample)
+    planilha = pd.read_csv(output, nrows=amostragem)
 
-    df = planilha['Fuel Economy'].value_counts()
+    for x in planilha['Model Year Range']:
 
-    cmap = matplotlib.cm.Blues
-    mini = min(df)
-    maxi = max(df)
-    norm = matplotlib.colors.Normalize(vmin=mini, vmax=maxi)
-    colors = [cmap(norm(i)) for i in df]
+        print("ITEM", x)
 
-    print(df)
-
-    squarify.plot(sizes=df, label=df.index)
-
-    pyplot.title('Economia de combustível por veículo.')
-
-    pyplot.show()
+    fig = px.sunburst(
+        planilha, path=['Company', 'Model',  'Price'])
+    fig.show()
 
 
-def grafico_de_linha(sample: int = 100):
+def grafico_de_linha(amostragem: int = 100):
     output = os.path.join(arquivo)
 
-    planilha = pd.read_csv(output, nrows=sample)
+    planilha = pd.read_csv(output, nrows=amostragem)
 
     pyplot.plot(
         planilha['Company'].unique(),
@@ -51,10 +46,10 @@ def grafico_de_linha(sample: int = 100):
     pyplot.show()
 
 
-def grafico_de_setores(sample: int = 100):
+def grafico_de_setores(amostragem: int = 100):
     output = os.path.join(arquivo)
 
-    planilha = pd.read_csv(output, nrows=sample)
+    planilha = pd.read_csv(output, nrows=amostragem)
 
     pyplot.pie(
         planilha.groupby('Company')['Model'].count(),
@@ -71,7 +66,7 @@ def grafico_de_setores(sample: int = 100):
 
 def main():
 
-    grafico_de_arvore()
+    grafico_sunburst()
 
     grafico_de_linha()
 
